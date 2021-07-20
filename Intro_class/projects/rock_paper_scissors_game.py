568213d0349078eb26dfbef6101aaa45b1fc078a
Derbew <derbew12@gmail.com>
2021-07-20 16:40:02 +0300

    Rock Paper scissors game Updated

--- Intro_class/projects/rock_paper_scissors_game.py
+++ Intro_class/projects/rock_paper_scissors_game.py
@@ -1,7 +1,9 @@
 from random import choice
 
 print("Rule of the game".center(60, "="))
-
+computer_sco=0
+player_sco=0
+count=0
 while True:
     poss_ac=["rock", "paper", "scissors"]
     user_guess=input("please enter one of these'rock' or 'paper' or 'scissors': ")
@@ -13,21 +15,35 @@ while True:
     elif user_guess=='rock':
         if comp_guess=='paper':
             print("Oops, You lose Computer won!")
+            computer_sco +=1
+            print(f"computer Won:{computer_sco}")
         else:
             print("Congradulations, You won!")
+            player_sco +=1 
+            print(f"You Won: {player_sco}")
     elif user_guess=='scissors':
         if comp_guess=='rock':
             print("Oops, You lose Computer won")
+            computer_sco +=1
+            print(f"computer Won:{computer_sco}")
         else:
             print("Congrats, You won!")
+            player_sco +=1 
+            print(f"You Won: {player_sco}")
     elif user_guess=='paper':
         if comp_guess=='scissors':
             print("Oops, You lose Computer won")
+            computer_sco +=1
+            print(f"computer Won:{computer_sco}")
         else:
-            print("Congrats, You won!") 
+            print("Congrats, You won!")
+            player_sco +=1 
+            print(f"You Won: {player_sco}")
     else:
         print("please enter correct value:")
     play_again=input("Do you want to play again y/n: ")
     play_again=play_again.upper()
     if play_again != 'Y':
         break
+    count += 1
+print (f"Total prompt: {count}")
